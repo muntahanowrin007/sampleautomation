25acf2dcad52887442a4cf8c418c4bbd6ec1a614
muntahanowrin007 <muntaha.nowrin007@gmail.com>
2021-11-29 01:25:47 +0100

    Drivers Added

--- src/test/java/com/fashionette/Initializer.java
+++ src/test/java/com/fashionette/Initializer.java
@@ -1,6 +1,8 @@
 package com.fashionette;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Parameters;
@@ -8,29 +10,45 @@ import org.testng.annotations.Parameters;
 public class Initializer {
 	public WebDriver driver;
 	public String applicationHost;
-	public String port;
 	public String browsers;
 	public String username;
 	public String password;
 	
 	
 	@BeforeSuite
-	@Parameters({ "applicationHost", "port", "browsers", "username", "password" })
-	public void beforeSuite(String applicationHost, String port, String browsers, String username, String password) {
+	@Parameters({ "applicationHost","browsers", "username", "password" })
+	public void beforeSuite(String applicationHost, String browsers, String username, String password) {
 		this.applicationHost = applicationHost;
-		this.port = port;
 		this.browsers = browsers;
 		this.username = username;
 		this.password = password;
 		
+		initializeDrivers();
+		openBrowser();
+		
+	}
+	public void initializeDrivers() {
+
+		String projectPath = System.getProperty("user.dir");
+
+		if(browsers.equalsIgnoreCase("firefox")) {
+			System.setProperty("webdriver.gecko.driver", projectPath + "\\drivers\\geckodriver\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else {
+			System.setProperty("webdriver.chrome.driver", projectPath + "\\drivers\\chromedriver\\chromedriver.exe");
+			driver = new ChromeDriver();
+		}
+
+	}
+	public void openBrowser() {
+		
 		System.out.println(applicationHost);
-		
-		
-		//driver =new FirefoxDriver();
+		driver.get(applicationHost);
+		driver.manage().window().maximize();
 	}
 
 	@AfterSuite
 	public void afterSuite() {
-		//driver.quit();
+		driver.quit();
 	}
 }
